29f9849396bf2a7e5d4f14c2db97fe118b22ee58
Gualtiero <gualtiero.frigerio@gmail.com>
2022-06-26 18:18:35 +0200

    added second link

--- SwiftUI4/Views/FirstPage.swift
+++ SwiftUI4/Views/FirstPage.swift
@@ -11,6 +11,9 @@ struct FirstPage: View {
     var body: some View {
         Text("This is the first page!")
         NavigationLink("goto to second", value: Destination.secondPage)
+        NavigationLink("go to second directly", destination: {
+            SecondPage()
+        })
     }
 }
 
